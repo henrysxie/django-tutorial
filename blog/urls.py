26234a3d4ff6db72dcc1afb8f40d17f3c42e21fe
Henry Xie <xie1989@gmail.com>
2015-05-20 22:30:04 -0400

    end of workshop

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+]
